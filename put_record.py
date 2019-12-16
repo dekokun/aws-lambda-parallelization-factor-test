609ae4339393e39deccdfbe3087a3c022cdb6f19
dekokun <dekokun@hatena.ne.jp>
2019-12-16 16:08:00 +0900

    引数をコマンド引数に

--- put_record.py
+++ put_record.py
@@ -10,8 +10,9 @@ client = session.client('kinesis')
 
 args = sys.argv
 streamName = args[1]
+recordCount = int(args[2])
 
-for i in range(500):
+for i in range(recordCount):
     data = {}
     data['ID'] = i
     data['TimeStamp'] = int(round(time.time()*1000))
